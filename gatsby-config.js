f68f58d3a3aeb1b0bba8bb896c74b25c5559c967
Artur Wróbel <artur.wrobel@gogoapps.io>
2020-01-08 12:38:23 +0100

    chore: setup plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "Gatsby Cvent"
   },
-  plugins: [
-    {
-      resolve: "gatsby-source-cvent",
-      options: {
-        account: "value",
-        username: "value",
-        password: "value"
-      }
+  plugins: [{
+    resolve: "gatsby-source-cvent",
+    options: {
+      account: process.env.CVENT_ACCOUNT,
+      username: process.env.CVENT_USERNAME,
+      password: process.env.CVENT_PASSWORD
     }
-  ]
+  }]
 }
